11c2a0f51194c537a48cdb1914c42a4f5d731981
cami2001 <camila.granda@ucuenca.edu.ec>
2023-05-05 21:07:34 -0500

    then.

--- KNVecinos.py
+++ KNVecinos.py
@@ -1,25 +1,24 @@
-import csv
 from tkinter import filedialog
 import tkinter as tk
+import pandas as pd
 
+df = pd.DataFrame()
+def lecturaArchivo():
 
-def lecturaArchivo(lista):
-
+    global df
     archivo_csv = filedialog.askopenfilename(filetypes=[("Archivo CSV", "*.csv")])
     # Leer el archivo CSV seleccionado
     try:
-        with open(archivo_csv) as archivo:
-            lector_csv = csv.reader(archivo)
-            next(lector_csv)
-            for fila in lector_csv:
-                lista.append(fila)
-            print("Datos ingresados correctamente")
+        datos = pd.read_csv(archivo_csv)
+        df = pd.concat([df, datos], ignore_index=True)
+        print("Datos ingresados correctamente")
+        return df
     except FileNotFoundError:
         print("No se eligió el archivo")
 
 def menu():
-    lista = []
     while True:
+        print("*******************Algoritmo de K-Vecinos*******************")
         print("1. Ingresar datos desde archivo .csv")
         print("2. Ingresar datos desde consola")
         print("3. Mostrar datos")
@@ -28,11 +27,13 @@ def menu():
             opc = int(input("Opcion: "))
             if opc == 1:
                 try:
+                    df.drop(index=df.index, columns=df.columns, inplace=True)
                     # Crear la ventana principal
                     root = tk.Tk()
 
                     # Agregar un botón para abrir el cuadro de diálogo de selección de archivos
-                    boton_abrir = tk.Button(root, text="Abrir archivo CSV", command=lambda: lecturaArchivo(lista))
+
+                    boton_abrir = tk.Button(root, text="Abrir archivo CSV", command=lecturaArchivo)
                     boton_abrir.pack()
 
                     # Mostrar la ventana principal
@@ -41,15 +42,10 @@ def menu():
                     print("No se eligió el archivo")
             elif opc == 2:
                 print("Ingresar datos desde consola")
-            elif opc == 3:
-                if len(lista) == 0:
-                    print("No hay datos para mostrar")
-                else:
-                    print("Mostrar datos: ")
-                    for i in range(len(lista)):
-                        print(lista[i])
-                    print("Cantidad de datos: ", len(lista))
+                print("Ingrese cantidad de variables")
 
+            elif opc == 3:
+                print(df)
             elif opc == 4:
                 print("Finalizando...")
                 break
